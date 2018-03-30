a6270ac9455585631e8517fdcf5bcfa81fba4d57
jensweatmon <jenjenhuynh@gmail.com>
2018-03-30 14:32:44 -0400

    removed keys

--- keys.js
+++ keys.js
@@ -1,26 +1,5 @@
 console.log('this is loaded');
 
-//Twitter
-var twitterkeys = {
-  consumer_key: 	ycDfFKceRutf8nXM0uI8KigxP,
-  consumer_secret: BhzXnyd7ZMaiMZRShvkMYkbeDUPC9cdSgEr1wkwz6SRyWwCgu3,
-  access_token_key: 976721974083969025-PVbOCTXCHHg5FX9rmUuHXeBb0x73CUv,
-  access_token_secret: Of0JDE1J475EKQSmCF263zmJtZ7IyWwgEzH0oyv2XnqA8
 
-};
 
-//Spotify
-var spotifykeys = {
-  id: f628b580ec874f1ab78887445dffbb8f,
-  secret: a45ea07037774e87b6ceffeb6ac61ca4
-};
-
-//OMDB
-var omdbkeys = "9ad71e7c";
-
-module.exports = {  //must use module.exports in order for it to be used in multiple .js
-	twitterkeys: twitterkeys,
-  spotifykeys: spotifykeys,
-  omdbkeys: omdbkeys
   
-};
--- liri.js
+++ liri.js
@@ -1,33 +1,62 @@
-//npm install for dotenv, request, twitter, spotify
+//npm install for dotenv, request, twitter, spotify, omdb
 
 require("dotenv").config();
 
 //retrieve keys.js file
-var Keys = require("./keys.js")
+var keys = require("./keys.js")
 //add variable for Twitter
 var Twitter = require('twitter');
 //add variable for Spotify
 var Spotify = require('node-spotify-api');
 //add variable for request
 var request = require('request');
+//add variable for read and write
+var fs = require("fs");
+//add variable for omdb
+var omdb = require('omdb');
 
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+
+switch (action) {
+  case "my-tweets":
+  myTweets();
+  break;
+
+  case "spotify-this-song":
+  mySpotify();
+  break;
+
+  case "movie-this":
+  myMovie();
+  break;
+
+  case "do-what-it-says":
+  itSays();
+  break;
+
+
+}
     
 // Twitter--------------
 
-function myTweets () {
-  var client = new Twitter(keys.twitter);
-      
-  var params = {screen_name: 'Node_Practice'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
-    }
-  });
-}
+function myTweets() {
 
+  var client = new Twitter(keys.twitterKeys);
+
+  var params = {screen_name: "Node_Practice1"};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+}
 
 // Spotify-----------
 
+function mySpotify() {
 
   var spotify = new Spotify(keys.spotify);
   
@@ -38,9 +67,10 @@ function myTweets () {
  
 console.log(data); 
 });
-
+}
 
 //OMDB--------------
+
 
 
 
